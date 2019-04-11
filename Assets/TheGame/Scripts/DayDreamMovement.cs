e6b238e6991ceb9964fbdd70bddf7e045758182f
kitty <wagnerk@uni-hildesheim.de>
2019-04-11 12:09:50 +0200

    project

---
+++ Assets/TheGame/Scripts/DayDreamMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+// Quelle: https://codingchronicles.com/unity-vr-development/day-62-of-100-days-of-vr-implementing-movement-in-google-daydream
+
+
+public class DayDreamMovement : MonoBehaviour
+{
+    private float _speedSlowDown;
+    private Camera camera;
+
+    void Start()
+    {
+        _speedSlowDown = 0.1f;
+        camera = Camera.main;
+    }
+
+    void Update()
+    {
+      camera.transform.position += new Vector3(0, 1, 0);
+        if (GvrControllerInput.IsTouching)
+        {
+            Debug.Log("Touchpad touched");
+            Vector3 touchPos = GvrControllerInput.TouchPos;
+            Vector3 movementVector = new Vector3(touchPos.x - 0.5f, 2, touchPos.y - 0.5f); //adjust for center to be at 0.5, 0.5
+ //           Vector3 playerPos = movementVector;
+ //           camera.transform.position = playerPos;
+            transform.Translate(movementVector.x * _speedSlowDown, 0, -movementVector.z * _speedSlowDown); // negative to adjust for the vector speed
+        }
+    }
+
+
+}
---
+++ Assets/TheGame/Scripts/DialogueManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Quelle: https://github.com/Brackeys/Dialogue-System
+
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text dialogueText;
+
+    private Queue<string> sentences;
+
+    public DialogueManager(Text dialogueText)
+    {
+        this.dialogueText = dialogueText;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("starting a conversation with " + dialogue.name);
+        string name = dialogue.name;
+        Debug.Log("Der Name ist " + name);
+
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+
+        if (dialogueText == null)
+        {
+            Debug.Log("dialogueText is null");
+            Debug.Log("The sentece is " + sentence);
+        }
+        else
+        {
+            dialogueText.text = sentence;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("End of dialogue");
+    }
+}
